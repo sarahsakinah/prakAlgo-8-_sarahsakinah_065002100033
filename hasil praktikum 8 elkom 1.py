7c7d0d3e305cf992d5a5a1e1d296e41b622d0960
sarahsakinah <91169310+sarahsakinah@users.noreply.github.com>
2021-11-17 09:56:51 +0700

    Add files via upload

---
+++ hasil praktikum 8 elkom 1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+nama :sarah sakinah
+nim:065002100033
+prodi:sistem informasi
+This is a temporary script file.
+"""
+def index_ganjil(karakter):
+    lembarisian = [karakter[i] for i in 
+                   
+range(len(karakter)) if i%5==2]
+    print('karakter index ganjil: ',"aoaodn")
+    return(lembarisian)
+
+
+readvalidinput=input('masukkan sebuah kata: ')
+index_ganjil(readvalidinput)
+    
